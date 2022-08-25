9176e33afbe8f18800ac241d8eace4eaa08d9724
emiperez997 <emi.perez997@gmail.com>
2022-08-24 21:14:18 -0300

    [ADD] Clase 43: Desafio Generico

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git pull https://emiperez997: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@github.com/No-Country/c6-43
+git pull https://emiperez997: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@github.com/emiperez997/Comision-36535
 
-echo Pusheado!
+echo Pulleado!
 
